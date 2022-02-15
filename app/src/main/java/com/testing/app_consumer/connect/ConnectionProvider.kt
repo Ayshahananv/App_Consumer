fbfe55915b1821e8a2f2c815ce8af61b4957c902
Ayshahananv <ayshahananvid@gmail.com>
2022-02-15 03:47:03 -0500

    commit

--- app/src/main/java/com/testing/app_consumer/connect/ConnectionProvider.kt
+++ app/src/main/java/com/testing/app_consumer/connect/ConnectionProvider.kt
@@ -2,96 +2,50 @@ package com.testing.app_consumer.connect
 
 import android.content.Context
 import android.util.Log
-import android.view.View
 import net.soti.xtsocket.ipc.IRequest
-import net.soti.xtsocket.ipc.IResponse
-import net.soti.xtsocket.ipc.controllers.TransportService
+import net.soti.xtsocket.ipc.controllers.XTSocket
+import net.soti.xtsocket.ipc.interfaces.Ipc
+import net.soti.xtsocket.ipc.interfaces.IpcService
 import net.soti.xtsocket.ipc.model.Request
 import org.json.JSONObject
-import java.lang.NullPointerException
 
-class ConnectionProvider:TransportService() {
-    private  val TAG = "Aysha"
-    val pkg= "com.testing.app_consumer"
-    var connected=false
-    private lateinit var iRequest:IRequest
-    var manufacturer:String=""
-    var model:String=""
-    var ip:String=""
-    var wifi:String=""
+class ConnectionProvider : IpcService {
+    private val TAG = "Aysha"
+    private val pkg = "com.testing.app_consumer"
+    private var connected = false
+    private lateinit var iRequest: IRequest
+    var manufacturer: String = ""
+    lateinit var ipc: Ipc
 
-    override fun onConnected() {
+    override fun onConnected(packageName: String, ipc: Ipc) {
+        this.ipc = ipc
         Log.d(TAG, "onConnected: ")
-
-        iRequest= iRequest()!!
-        //for (i in 1..100000) {
+        iRequest = ipc.iRequest()!!
         getSchema()
-
-        //}
-//        val s:String ="start_time"
-//        val e:String="end_time"
-//        val st:String="1636976059940"
-//        val et:String="1636976060976"
-        if (connected){
-            manufacturer=getIRequest().requestData(
-                pkg, arrayListOf(Request("manufacturer", JSONObject().toString()))
+        if (connected) {
+            manufacturer = iRequest.requestData(
+                pkg, listOf(Request("level", JSONObject().toString()))
             ).toString()
-            model=getIRequest().requestData(
-                pkg, arrayListOf(Request("model", JSONObject().toString()))
-            ).toString()
-            ip=getIRequest().requestData(
-                pkg, arrayListOf(Request("ip", JSONObject().toString()))
-            ).toString()
-            //wifi=getIRequest().requestData(pkg, arrayListOf(Request("wifi_status",JSONObject().toString()))).toString()
         }
-       registerEventReceiver(pkg, EventReceiver())
-
-        Log.d("Aysha manufacturer", manufacturer)
-        Log.d("Aysha model", model)
-        Log.d("Aysha ip", ip)
-       //Log.d("Aysha wifi", wifi)
-    }
-    fun getIRequest():IRequest{
-        return iRequest
+        Log.d(TAG, "manu: $manufacturer")
     }
 
-    fun getSchema() {
-            if (connected) {
-                val schema = getIRequest().schema
-                Log.d("Aysha schema", "onSchema: ${JSONObject(schema!!)}")
-                val requiredFeature = JSONObject(schema).keys().asSequence().toMutableList()
-                Log.d("aysha", "getSchema: required: $requiredFeature")
-                //subscribe
-                getIRequest().subscribe(pkg, requiredFeature)
-            }
+    private fun getSchema() {
+        if (connected) {
+            val schema = JSONObject(iRequest.schema!!.value!!)
+            Log.e(TAG, "onSchema: ${JSONObject(iRequest.schema!!.value!!)}")
+            val requiredFeature =
+                schema.keys().asSequence().toList()
+            iRequest.subscribe(pkg, requiredFeature)
+        }
     }
-//    fun getSchema() {
-//        if (connected) {
-//            val schema = getIRequest().schema
-//            Log.d("kajal", "getSchema:$schema ")
-//            Log.e(TAG, "onSchema: ${JSONObject(schema!!)}")
-////            for testing only
-//            val requiredFeature =
-//                JSONObject(JSONObject(schema).get("schema").toString()).keys().asSequence()
-//                    .toMutableList()
-//            //requiredFeature.remove("wifi_status")
-//            getIRequest().subscribe(pkg, requiredFeature)
-//           // onSchemaReceived(true)
-//        }
-//    }
+
     override fun onDisconnected() {
-        Log.d("Aysha disconnect", "onDisconnected: ")
-
-
+        Log.d(TAG, "onDisconnected: ")
     }
-    fun connect(context:Context){
-        Log.d("Aysha", "connect: Started")
-        connected=connectProvider(context,"com.testing.app_producer",this)
-        Log.d("Aysha connected", connected.toString())
-    }
-}
-class EventReceiver:IResponse.Stub(){
-    override fun onEvent(p0: String?) {
-        Log.d("Aysha", "onEvent: ${JSONObject(p0!!)}")
+
+    fun connect(context: Context) {
+        connected = XTSocket().connect(context, "com.zebra.smartbattery", this)
+        Log.d(TAG,"connected${connected.toString()}")
     }
 }
